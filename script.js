34c88514b464a77630809e8d0d3a5180c265ea8a
ANMOL <43845658+Anmol270900@users.noreply.github.com>
2019-06-21 11:45:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+var rock = document.querySelector("#rock");
+var paper = document.querySelector("#paper");
+var scissors = document.querySelector("#scissors");
+var userChoice;
+var computerChoice;
+
+function compChoice()
+    {   
+        console.log("inside the computer choice fn");
+        computerChoice = Math.random();
+
+        if (computerChoice < 0.34) 
+        {
+            alert("computer chose rock");
+            return "rock";
+        } 
+        else 
+        if(computerChoice <= 0.67) 
+        {
+            alert("computer chose paper");
+            return "paper";
+        } 
+        else 
+        {
+            alert("computer chose scissors");
+            return "scissors";
+        }
+    }
+
+function usChoice() 
+	{
+		rock.addEventListener("click", () => {
+            userChoice = "rock";
+		    alert("you chose rock");
+            console.log(userChoice);
+            //return "rock";
+
+    	});
+
+	    paper.addEventListener("click", () => {
+
+	        userChoice = "paper";
+            alert("you chose paper");
+            console.log(userChoice);
+            //return "paper";
+	    });
+
+	    scissors.addEventListener("click", () => {
+	        userChoice = "scissors";
+            alert("you chose scissors");
+            console.log(userChoice);
+            //return "scissors";
+	    });
+	}
+
+function compare(choice1, choice2) 
+{
+    if(choice1 === choice2) 
+    {
+        return "The result is a tie!";
+    }
+    if(choice1 === "rock") 
+    {
+        if(choice2 === "scissors") 
+        {
+            return "rock wins";
+        } 
+        else 
+        {
+            return "paper wins";
+        }
+    }
+    if(choice1 === "paper") 
+    {
+        if(choice2 === "rock") 
+        {
+            return "paper wins";
+        } 
+        else 
+        {
+            if(choice2 === "scissors") 
+            {
+                return "scissors wins";
+            }       
+        }
+        if(choice1 === "scissors") 
+        {
+            if(choice2 === "rock") 
+            {
+                return "rock wins";
+            } 
+            else 
+            {
+                if(choice2 === "paper") 
+                {
+                    return "scissors wins";
+                }
+            }
+        }
+    }
+};
+
+
+//userChoice = usChoice();
+usChoice();
+if(userChoice !== undefined)
+{
+    computerChoice = compChoice();
+    alert(compare(userChoice, computerChoice));
+}
