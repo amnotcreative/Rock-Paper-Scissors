fb2185dff2efb21e29ac4958db311bf8e5d510eb
Gautami Sinha <gautami.sinha90@gmail.com>
2022-10-22 19:52:58 +0530

    The game as Sheldon sees it

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var rock = document.querySelector("#rock");
 var paper = document.querySelector("#paper");
 var scissors = document.querySelector("#scissors");
+var lizard = document.querySelector("#lizard");
+var spock = document.querySelector("#spock");
 var userChoice;
 var computerChoice;
 
@@ -9,21 +11,30 @@ function compChoice()
         console.log("inside the computer choice fn");
         computerChoice = Math.random();
 
-        if (computerChoice < 0.34) 
+        if (computerChoice < 0.20) 
         {
             alert("computer chose rock");
             return "rock";
         } 
-        else 
-        if(computerChoice <= 0.67) 
+        else if(computerChoice <= 0.40) 
         {
             alert("computer chose paper");
             return "paper";
         } 
-        else 
+        else if(computerChoice <= 0.60) 
         {
             alert("computer chose scissors");
             return "scissors";
+        } 
+        else if(computerChoice <= 0.80) 
+        {
+            alert("computer chose lizard");
+            return "lizard";
+        } 
+        else 
+        {
+            alert("computer chose spock");
+            return "spock";
         }
     }
 
@@ -42,9 +53,21 @@ function usChoice()
 		    
 	    });
 
-	    scissors.addEventListener("click", () => {
+        scissors.addEventListener("click", () => {
             alert("you chose scissors");
 		    alert(compare("scissors", compChoice()));
+		    
+	    });
+
+        lizard.addEventListener("click", () => {
+            alert("you chose lizard");
+		    alert(compare("lizard", compChoice()));
+		    
+	    });
+
+	    spock.addEventListener("click", () => {
+            alert("you chose spock");
+		    alert(compare("spock", compChoice()));
 	    });
 	}
 
@@ -54,45 +77,67 @@ function compare(choice1, choice2)
     {
         return "The result is a tie!";
     }
+
     if(choice1 === "rock") 
     {
-        if(choice2 === "scissors") 
+        if(choice2 === "scissors" || choice2 === "lizard")
         {
-            return "rock wins";
+            return "you win";
         } 
         else 
         {
-            return "paper wins";
+            return "computer wins";
         }
     }
-    if(choice1 === "paper") 
+
+    if(choice1 === "paper" ) 
     {
-        if(choice2 === "rock") 
+        if(choice2 === "rock" || choice2 === "spock") 
         {
-            return "paper wins";
+            return "you win";
         } 
         else 
         {
-            if(choice2 === "scissors") 
-            {
-                return "scissors wins";
-            }       
+                return "computer wins";       
         }
     }
+
         if(choice1 === "scissors") 
         {
-            if(choice2 === "rock") 
+            if(choice2 === "paper" || choice2 === "lizard") 
             {
-                return "rock wins";
+                return "you win";
             } 
             else 
             {
-                if(choice2 === "paper") 
-                {
-                    return "scissors wins";
-                }
+                    return "computer wins";
             }
         }
+
+        if(choice1 === "lizard") 
+        {
+            if(choice2 === "paper" || choice2 === "spock") 
+            {
+                return "you win";
+            } 
+            else 
+            {
+                    return "computer wins";
+            }
+        }
+
+        if(choice1 === "spock") 
+        {
+            if(choice2 === "scissors" || choice2 === "rock") 
+            {
+                return "you win";
+            } 
+            else 
+            {
+                    return "computer wins";
+            }
+        }
+
 };
 
 
